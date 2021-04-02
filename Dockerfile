bbf5dd2bf040bfdc37af6a20159e2e3a12c4727e
Zack <aminuzack7@gmail.com>
2021-04-02 13:19:58 +0100

    added swagger docs and edited tsconfig.json

--- Dockerfile
+++ Dockerfile
@@ -23,7 +23,7 @@ COPY --from=builder /app/node_modules ./node_modules
 COPY --from=builder /app/package*.json ./
 COPY --from=builder /app/dist ./dist
 
-EXPOSE 3000
+EXPOSE 5000
 CMD [ "npm", "run", "start:prod" ]
 
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerCustomOptions, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true
+  }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Digital Escrow API')
+    .setDescription('Digital Escrow API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addTag('api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  const customOptions: SwaggerCustomOptions = {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+    customSiteTitle: 'Digital Escrow API Docs',
+  };
+
+  SwaggerModule.setup('api/v1', app, document, customOptions);
+
+  app.enableCors();
+
+  await app.listen(process.env.PORT || 5000);
+
+  console.log(`server running on ${await app.getUrl()} : ` + new Date());
 }
+
 bootstrap();
