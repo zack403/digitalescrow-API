a5a696f1d74032f1d05bd658c517900559ccbf9c
Zack <aminuzack7@gmail.com>
2021-04-14 17:54:55 +0100

    change db url

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -9,11 +9,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
           inject: [ConfigService],
           useFactory: (configService: ConfigService) => ({
             type: 'postgres',
-            host: configService.get('POSTGRES_HOST'),
-            port: configService.get('POSTGRES_PORT'),
-            username: configService.get('POSTGRES_USER'),
-            password: configService.get('POSTGRES_PASSWORD'),
-            database: configService.get('POSTGRES_DB'),
+            url: configService.get('DATABASE_URL'),
             entities: [
               __dirname + '/../**/*.entity.ts',
               __dirname + '/../**/*.entity.js',
